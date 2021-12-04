73b2982fa0f5dfa41ab66f9ee31773d0351014cb
Yoseob Kim <ykim9611@gmail.com>
2021-12-04 05:30:07 -0800

    Implemented adding new production run feature

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const requestHandler = require('./Controller/index.js').requestHandler;
 
 app.use(express.json());
 app.use(express.static(__dirname + '/../dist/'))
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 
 app.get('/openProductionList', requestHandler.openProductionList);
 app.get('/partsList/:id', requestHandler.partsList);
-
+app.post('/addNewProductionRun', requestHandler.addNewProductionRun);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
