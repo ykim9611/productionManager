454ce9efa50930c6e8308d791992adbfb5ed02a3
Yoseob Kim <ykim9611@gmail.com>
2021-12-03 22:59:02 -0800

    Initial Schema design

---
+++ Schema.sql
@@ -0,0 +1,23 @@
+-- run mysql -u root < Schema.sql
+
+DROP DATABASE IF EXISTS productionManager;
+CREATE DATABASE productionManager;
+USE productionManager;
+
+CREATE TABLE productionRun (
+  id INT NOT NULL AUTO_INCREMENT,
+  productName VARCHAR(255) NOT NULL,
+  annualSales INT NOT NULL,
+  prodStartDate DATE NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE parts (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_id INT NOT NULL,
+  eta DATE NOT NULL,
+  received BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE parts ADD FOREIGN KEY (product_id) REFERENCES productionRun (id);
