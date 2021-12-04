931e6cafa52007c0b94869e736d894df4c284e75
Yoseob Kim <ykim9611@gmail.com>
2021-12-03 17:05:41 -0800

    Added css modules

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,19 @@ module.exports = {
           }
         }
       },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
     ]
   },
   plugins: [
