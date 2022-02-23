bcde3cf63423b52a2c158225185544cffa874a92
Yoseob Kim <ykim9611@gmail.com>
2022-02-22 23:29:16 -0800

    converted App class component to functional component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,68 +1,32 @@
 import React, { useState, useEffect } from "react";
 import styles from "./styles.css";
 import axios from "axios";
-import NewProduction from "./NewProductionForm/NewProduction.jsx";
+import NewProductionForm from "./NewProductionForm/NewProductionForm.jsx";
 import OpenProductionList from "./ProductionList/OpenProductionList.jsx";
 import UpcomingProductionList from "./UpcomingProductionList/UpcomingProductionList.jsx";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      openProductionList: [],
-      upcomingProductionList: [],
-    };
-  }
-  componentDidMount() {
-    this.getOpenProductionList();
-  }
+export default function App() {
+  const [openProductionList, setOpenProductionList] = useState([]);
+  const [upcomingProductionList, setUpcomingProductionList] = useState([]);
 
-  getOpenProductionList() {
+  useEffect(() => {
+    getOpenProductionList();
+  }, []);
+
+  function getOpenProductionList() {
     axios.get("/openProductionList").then(({ data }) => {
-      console.log(data);
-      this.setState({ openProductionList: data });
+      setOpenProductionList(data);
     });
   }
-
-  render() {
-    return (
-      <div className={styles.titleSection}>
-        <h1>Production Manager</h1>
-        <NewProduction getAll={this.getOpenProductionList.bind(this)} />
-        <OpenProductionList
-          list={this.state.openProductionList}
-          getAll={this.getOpenProductionList.bind(this)}
-        />
-        {/* <UpcomingProductionList list={this.state.upcomingProductionList}/> */}
-      </div>
-    );
-  }
+  return (
+    <div className={styles.titleSection}>
+      <h1>Production Manager</h1>
+      <NewProductionForm getAll={getOpenProductionList} />
+      <OpenProductionList
+        list={openProductionList}
+        getAll={getOpenProductionList}
+      />
+      <UpcomingProductionList list={upcomingProductionList} />
+    </div>
+  );
 }
-
-// function App() {
-//   const [openProductionList, setOpenProductionList] = useState([]);
-
-//   useEffect(() => {
-//     getOpenProductionList();
-//   }, []);
-
-//   function getOpenProductionList() {
-//     axios.get("/openProductionList").then(({ data }) => {
-//       console.log(data);
-//       setOpenProductionList(data);
-//     });
-//   }
-//   return (
-//     <div className={styles.titleSection}>
-//       <h1>Production Manager</h1>
-//       <NewProduction getAll={getOpenProductionList} />
-//       <OpenProductionList
-//         list={openProductionList}
-//         getAll={getOpenProductionList}
-//       />
-//       {/* <UpcomingProductionList list={this.state.upcomingProductionList}/> */}
-//     </div>
-//   );
-// }
-
-export default App;
