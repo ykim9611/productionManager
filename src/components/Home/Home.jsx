d71cc4d21aa0d64ae35af9cd7e4e3caf87f37274
Yoseob Kim <ykim9611@gmail.com>
2022-02-23 15:08:28 -0800

    added separate home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <h2>Welcome to Production Manager!</h2>
+      <p>
+        You can simply manage all your open production runs and never miss any
+        due dates!
+      </p>
+      <div>Author: Yoseob Kim</div>
+    </>
+  );
+}
