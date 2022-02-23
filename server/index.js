849d10814cce41eecdd46b474e409f96f5cdb46e
Yoseob Kim <ykim9611@gmail.com>
2022-02-23 14:42:20 -0800

    configured reactor router

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 const requestHandler = require("./Controller/index.js").requestHandler;
@@ -19,6 +20,14 @@ app.patch("/editLeadTime", requestHandler.editLeadTime);
 app.patch("/updateTotalLeadTime", requestHandler.updateTotalLeadTime);
 app.patch("/updateReceived", requestHandler.updateReceived);
 app.patch("/updateBool", requestHandler.updateBool);
+//Forward all react-router routes to index.html
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/../dist/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 
 app.listen(port, (err) => {
   if (err) console.log("Error in server setup");
